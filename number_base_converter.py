93aef2ce7ae8b94eb7a81598b879e0a602c90984
𝕄𝕒𝕥𝕥𝕙𝕖𝕨-𝟜𝟝𝟞 <eusoumatheusfernandes@gmail.com>
2024-11-28 23:44:54 -0300

    Update number_base_converter.py

--- number_base_converter.py
+++ number_base_converter.py
@@ -1,14 +1,29 @@
-n = int(input("Enter any number: "))
-print("""Choose one of the options for Base conversion: 
-      [ 1 ] Convert to BINARY
-      [ 2 ] Convert to OCTAL
-      [ 3 ] Convert to HEXADECIMAL""")
-option = int(input("Your choice: "))
-if option == 1:
-    print("The number {} converted to BINARY is {}".format(n, bin(n)[2:]))
-elif option == 2:
-    print("The number {} converted to OCTAL is {}".format(n, oct(n)[2:]))
-elif option == 3:
-    print("The number {} converted to HEXADECIMAL is {}".format(n, hex(n)[2:]))
-else:
-    print("Invalid option!")
+# Welcome message
+print("🎉 Welcome to the Number Converter! 🎉")
+print("Here, you can transform numbers into different bases!")
+print("Choose a number and an option to see the magic happen! ✨")
+print("Ready to get started? Let's go! 🚀\n")
+
+while True:
+    n = int(input("Enter any number: "))
+    print("""Choose one of the options for Base conversion: 
+          [ 1 ] Convert to BINARY
+          [ 2 ] Convert to OCTAL
+          [ 3 ] Convert to HEXADECIMAL""")
+    option = int(input("Your choice: "))
+    
+    if option == 1:
+        print("The number {} converted to BINARY is {}".format(n, bin(n)[2:]))
+    elif option == 2:
+        print("The number {} converted to OCTAL is {}".format(n, oct(n)[2:]))
+    elif option == 3:
+        print("The number {} converted to HEXADECIMAL is {}".format(n, hex(n)[2:]))
+    else:
+        print("Invalid option!")
+
+    # Ask if the user wants to continue
+    continue_choice = input("Do you want to convert another number? (yes/no): ").strip().lower()
+    if continue_choice != 'yes':
+        print("Thank you for using the Number Converter! Goodbye! 🎉")
+        break
+
